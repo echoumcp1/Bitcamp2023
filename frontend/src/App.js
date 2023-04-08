23fd9a08b2c1f56444f6d2b49274cda58230d8b4
bzhang <scentedsoap>
2023-04-08 04:38:15 -0400

    added read csv

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,34 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-  const [result, setResult] = useState("");
-  useEffect(() => {
-    const myfunc = async () => {
-      const test = await axios.get("/");
-      setResult(test)
-    }
-    
-    myfunc();
-  }, []);
+  const [csvData, setCsvData] = useState();
 
+  /* this handleCsvFileUpload gets called whenever a csv file gets uploaded
+    using the onChange event listener */
+  const handleCsvFileUpload = (event) => {
+    console.log(event)
+    const file = event.target.files[0] // should be the file that is being uploaded
+    const reader = new FileReader() // built-in javascript object that provides a way to the read contents 
+
+    /* onload event is fired when a file is loaded into a FileReader()
+     * 
+    */
+    reader.onload = (event) => {
+      const csvText = event.target.result
+      setCsvData(csvText) 
+    }
+
+    /* the readAsText() function takes in a file and reads its contents
+     * the onload event is fired when the contents of the file have been loaded into memory
+     * 
+     */
+    reader.readAsText(file)
+  }
   return (
     <div className="App">
-        <h1>{result}</h1>
+      <input id="csv-file" type="file" accept=".csv" onChange={handleCsvFileUpload}></input>
+      <input type = "submit" value="Submit" ></input>
+      {csvData && <p>{csvData}</p>}
     </div>
   );
 }
