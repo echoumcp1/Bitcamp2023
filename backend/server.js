7e8a27775066bdce135f4f8161009986253a55f1
bzhang <scentedsoap>
2023-04-08 16:30:23 -0400

    added testing data

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,6 @@ const openai = new OpenAIApi(configuration);
 
 const response = async ()=> {
     const result = await openai.createCompletion({
-<<<<<<< HEAD
-        model: "text-davinci-003",
-        prompt: "Decide whether this payment is tax credit eligible? why? \n\nPayment: \"University of Maryland\"\nYes or No, tax credit eligible:",
-        max_tokens: 30
-        });
-=======
       model: "ada:ft-personal-2023-04-08-08-48-58",
       prompt: "University of Maryland\n\n###\n\n",
       temperature: 0,
@@ -31,7 +25,6 @@ const response = async ()=> {
       frequency_penalty: 2,
       presence_penalty: -2,
     });
->>>>>>> bb78102 (first training)
     return result.data.choices[0].text;
     };
 
