8c34c38283184f0ec41ac5251c7d22be40ca4300
bzhang <scentedsoap>
2023-04-08 00:56:10 -0400

    errors are rampent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    const myfunc = async () => {
+      const test = await axios.get("/");
+      setResult(test)
+    }
+    
+    myfunc();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{result}</h1>
     </div>
   );
 }
---
+++ frontend/src/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Input() {
+    return (
+        <input id="csv-upload" type="file" accept=".csv"></input>
+        
+    )
+}
