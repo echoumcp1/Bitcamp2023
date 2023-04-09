673147be07726bb8014f665690c37a25a9ff0c6a
Anna Phung <aphung1@umd.edu>
2023-04-09 07:12:57 -0400

    Added pie charts and fixed layouts

--- frontend/src/Pages/Details.js
+++ frontend/src/Pages/Details.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Table from "react-bootstrap/Table";
 import { CgProfile } from "react-icons/cg";
+import { Bar, Pie } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
+
 import {
   calculateCollegeCredit,
   calculateDependentCredits,
@@ -18,6 +21,7 @@ import Papa from "papaparse";
 import "./Details.css";
 
 const Details = () => {
+  ChartJS.register(...registerables);
   // const [result, setResult] = useState("");
 
   // useEffect(() => {
@@ -57,22 +61,23 @@ const Details = () => {
   const { state } = useLocation();
   const { income, school, dependants, csvData, witholding, status, medical } =
     state;
+
   const parsedData = Papa.parse(csvData).data;
+  const [completeData, setCompleteData] = useState([...parsedData]);
+  const [barData, setBarData] = useState([0, 0, 0, 0, 0]);
+  const [pieData, setPieData] = useState([0, 0, 0]);
+
+  console.log(`Income: ${income * (witholding / 100)}`);
+
+  const [doneParsing, setDoneParsing] = useState(false);
 
   const [taxReturn, setTaxReturn] = useState({
-    totalIncome: income * (witholding/100),
+    totalIncome: income * (witholding / 100),
     totalDeductions: 0,
     totalDonationDeductions: 0,
     totalCredits: 0,
     taxLiability: 0,
   });
-
-  /*
-
-  useEffect(() => {
-    console.log("TaxReturn", JSON.stringify(taxReturn));
-  }, [taxReturn]);
-*/
 
   /* this section below calculates the tax liability if the person were to take a
     standard deduction based instead of itemized, I have not fully tested! */
@@ -118,15 +123,21 @@ const Details = () => {
   useEffect(() => {
     const processResults = async () => {
       console.log("Hello");
-      for (let payment of parsedData) {
+      for (let [i, payment] of parsedData.entries()) {
         let payee = payment[0]; //the payee for each payment, need to send to GPT for them to classify
         /* send info to gpt here and get the array [yes/no, credit/deductions, category]*/
         let response = await axios.post("http://localhost:2000/api", { payee });
         let completePayment = response.data;
         completePayment.push(payment[1]);
+        let newData = [...parsedData];
+        if (response.data[1] == "Credit" || response.data[1] == "Deduction") {
+          newData[i].push(response.data[1]);
+          newData[i].push(response.data[2]);
+        }
+        console.log(`newData: ${newData}`);
+        setCompleteData(newData);
         //console.log(completePayment.toString());
         calculate(completePayment.toString());
-        //console.log(`total Credits ${taxReturn.totalCredits}`);
       }
       //TO-DO: Calculate the tax liability (subtract deductions -> find tax -> minus credits)
       const taxableIncome = taxReturn.totalIncome - taxReturn.totalDeductions;
@@ -136,7 +147,39 @@ const Details = () => {
       if (dependants > 0) {
         //calculateDependentCredits(status, income, dependants, taxLiability);
       }
-      setTaxReturn((oldTaxReturn) => {return {...oldTaxReturn, taxLiability: taxLiability}})
+      setTaxReturn((oldTaxReturn) => {
+        return { ...oldTaxReturn, taxLiability: taxLiability };
+      });
+      setDoneParsing(true);
+
+      let newBarData = [0, 0, 0, 0, 0];
+      let newPieData = [0, 0, 0];
+      completeData.forEach((row) => {
+        for (let [i, category] of [
+          "Education",
+          "Mortage Interest",
+          "Donations",
+          "Taxes",
+          "Medical",
+        ].entries()) {
+          if (row[3] == category) {
+            newBarData[i] += 1;
+          }
+        }
+
+        if (row[2] == "Credit") {
+          newPieData[1] += 1;
+        } else if (row[2] == "Deduction") {
+          newPieData[2] += 1;
+        } else {
+          newPieData[0] += 1;
+        }
+      });
+
+      setBarData(newBarData);
+      setPieData(newPieData);
+      console.log(`New Bar Data: ${newBarData}`);
+      console.log(`New Pie Data: ${newPieData}`);
     };
 
     processResults();
@@ -174,7 +217,7 @@ const Details = () => {
               school
             )}`
           );*/
-          //console.log(`NewCredits: ${newCredit}`);
+          console.log(`NewCredits: ${newCredit}`);
           //console.log(school)
           const newTaxReturn = { ...taxReturn, totalCredits: newCredit };
           //console.log(`oldTaxReturn: ${JSON.stringify(taxReturn)}`);
@@ -194,14 +237,24 @@ const Details = () => {
               calculateMedicalDeductions(taxReturn.totalIncome, medical);
             break;
           case Category.Donation:
-  
-              let currDonationDeduction = calculateDonationDeductions(taxReturn.totalIncome, taxReturn.totalDonationDeductions, amount);
-              newDeduction = taxReturn.totalDeductions + currDonationDeduction
-            setTaxReturn((oldTaxReturn) => {return {...oldTaxReturn, totalDonationDeductions: oldTaxReturn.totalDonationDeductions+currDonationDeduction}})
+            let currDonationDeduction = calculateDonationDeductions(
+              taxReturn.totalIncome,
+              taxReturn.totalDonationDeductions,
+              amount
+            );
+            newDeduction = taxReturn.totalDeductions + currDonationDeduction;
+            setTaxReturn((oldTaxReturn) => {
+              return {
+                ...oldTaxReturn,
+                totalDonationDeductions:
+                  oldTaxReturn.totalDonationDeductions + currDonationDeduction,
+              };
+            });
             break;
           case Category.Taxes:
             newDeduction =
-              taxReturn.totalDeductions + calculateTaxDeductions(status, amount);
+              taxReturn.totalDeductions +
+              calculateTaxDeductions(status, amount);
             break;
           case Category.MInterest:
             newDeduction =
@@ -226,81 +279,143 @@ const Details = () => {
   return (
     <div className="Details">
       <Navbar />
-      <div className="dashboard">
-        <div className="cardProfile">
-          <div className="profileWrapper">
-            <CgProfile size={150} />
+      {doneParsing && (
+        <div className="dashboard">
+          <div className="cardProfile">
+            <div className="profileWrapper">
+              <CgProfile size={150} />
+            </div>
+            <div className="grow">
+              <table>
+                <tr>
+                  <td>Gross Annual Income: ${income}</td>
+                  <td>School: {school}</td>
+                </tr>
+                <tr>
+                  <td>Dependants: {dependants}</td>
+                  <td>Witholding: {witholding}</td>
+                </tr>
+                <tr>
+                  <td>Status: {status}</td>
+                  <td>Medical: {medical}</td>
+                </tr>
+              </table>
+            </div>
           </div>
-          <div className="grow">
-            <table>
-              <tr>
-                <td>Gross Annual Income: ${income}</td>
-                <td>School: {school}</td>
-              </tr>
-              <tr>
-                <td>Dependants: {dependants}</td>
-                <td>Witholding: {witholding}</td>
-              </tr>
-              <tr>
-                <td>Status: {status}</td>
-                <td>Medical: {medical}</td>
-              </tr>
-            </table>
+          <div className="cardSummary">
+            <h2>Total Deductions:</h2>
+            <h3>{taxReturn.totalDeductions}</h3>
+            <h2>Total Credits:</h2>
+            <h3>{taxReturn.totalCredits}</h3>
+            <h2>Total Tax Liability:</h2>
+            <h3>{taxReturn.taxLiability}</h3>
+          </div>
+          <div className="cardBar">
+            <div>
+              <Bar
+                data={{
+                  labels: [
+                    "College",
+                    "Mortgage Interest",
+                    "Donations",
+                    "Taxes",
+                    "Medical",
+                  ],
+                  datasets: [
+                    {
+                      label: "Distribution of Credits/Deductions",
+                      backgroundColor: "rgba(75,192,192,1)",
+                      borderColor: "rgba(0,0,0,1)",
+                      borderWidth: 2,
+                      data: barData,
+                    },
+                  ],
+                }}
+                options={{
+                  title: {
+                    display: true,
+                    text: "Average Rainfall per month",
+                    fontSize: 20,
+                  },
+                  legend: {
+                    display: true,
+                    position: "right",
+                  },
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="cardPie">
+            <div>
+              <Pie
+                data={{
+                  labels: ["Ineligible", "Credit", "Deduction"],
+                  datasets: [
+                    {
+                      label: "Distribution of Transactions",
+                      data: pieData,
+                      backgroundColor: [
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(54, 162, 235, 0.2)",
+                        "rgba(255, 206, 86, 0.2)",
+                      ],
+                      borderColor: [
+                        "rgba(255, 99, 132, 1)",
+                        "rgba(54, 162, 235, 1)",
+                        "rgba(255, 206, 86, 1)",
+                      ],
+                      borderWidth: 1,
+                    },
+                  ],
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="cardItem">
+            <p>BarData {barData}</p>
+          </div>
+          <div className="cardItem">
+            <p>Dependants {dependants}</p>
+          </div>
+          <div className="cardItem">
+            <p>Witholding {witholding}</p>
+          </div>
+          <div className="cardItem">
+            <p>Status {status}</p>
+          </div>
+          <div className="cardItem">
+            <p>Medical {medical}</p>
+          </div>
+
+          <div className="cardTable">
+            <Table striped>
+              <thead>
+                <tr>
+                  {["Source", "Payment", "Credit/Deduction", "Category"].map(
+                    (header, i) => (
+                      <th key={`header${i}`}>{header}</th>
+                    )
+                  )}
+                </tr>
+              </thead>
+              <tbody>
+                {completeData &&
+                  completeData.slice(1).map((row, i) => {
+                    return (
+                      <tr key={`column${i}`}>
+                        {row.slice(0, 4).map((entry, j) => (
+                          <td key={`column${i}row${j}`}>{entry}</td>
+                        ))}
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
           </div>
         </div>
-        <div className="cardSummary">
-          <h2>Total Deductions:</h2>
-          <h3>{taxReturn.totalDeductions}</h3>
-          <h2>Total Credits:</h2>
-          <h3>{taxReturn.totalCredits}</h3>
-          <h2>Total Tax Liability:</h2>
-          <h3>{taxReturn.taxLiability}</h3>
-        </div>
-        <div className="cardItem">
-          <p>Gross Annual Income: ${income}</p>
-        </div>
-
-        <div className="cardItem">
-          <p>School {school}</p>
-        </div>
-        <div className="cardItem">
-          <p>Dependants {dependants}</p>
-        </div>
-        <div className="cardItem">
-          <p>Witholding {witholding}</p>
-        </div>
-        <div className="cardItem">
-          <p>Status {status}</p>
-        </div>
-        <div className="cardItem">
-          <p>Medical {medical}</p>
-        </div>
-
-        <div className="cardTable">
-          <Table striped>
-            <thead>
-              <tr>
-                {parsedData &&
-                  parsedData[0].map((header, i) => (
-                    <th key={`header${i}`}>{header}</th>
-                  ))}
-              </tr>
-            </thead>
-            <tbody>
-              {parsedData &&
-                parsedData.slice(1).map((row, i) => {
-                  return (
-                    <tr key={`column${i}`}>
-                      {row.map((entry, j) => (
-                        <td key={`column${i}row${j}`}>{entry}</td>
-                      ))}
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </Table>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
