a985f053e645a7e2a1e6cc4b0810b626f3aa5488
Anna Phung <aphung1@umd.edu>
2023-04-09 00:22:47 -0400

    Making the dashboard include the profie

--- frontend/src/Pages/Details.js
+++ frontend/src/Pages/Details.js
@@ -16,35 +16,69 @@ const Details = () => {
     <div className="Details">
       <Navbar />
       <div className="dashboard">
-        <p>Income {income}</p>
-        <p>School {school}</p>
-        <p>Dependants {dependants}</p>
-        <p>Witholding {witholding}</p>
-        <p>Status {status}</p>
-        <p>Medical {medical}</p>
+        <div className="cardProfile">
+          <div></div>
+          <div>
+            <table>
+              <tr>
+                <td>Gross Annual Income: ${income}</td>
+                <td>School: {school}</td>
+              </tr>
+              <tr>
+                <td>Dependants: {dependants}</td>
+                <td>Witholding: {witholding}</td>
+              </tr>
+              <tr>
+                <td>Status: {status}</td>
+                <td>Medical: {medical}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="cardItem">
+          <p>Gross Annual Income: ${income}</p>
+        </div>
 
-        <Table striped>
-          <thead>
-            <tr>
+        <div className="cardItem">
+          <p>School {school}</p>
+        </div>
+        <div className="cardItem">
+          <p>Dependants {dependants}</p>
+        </div>
+        <div className="cardItem">
+          <p>Witholding {witholding}</p>
+        </div>
+        <div className="cardItem">
+          <p>Status {status}</p>
+        </div>
+        <div className="cardItem">
+          <p>Medical {medical}</p>
+        </div>
+
+        <div className="cardTable">
+          <Table striped>
+            <thead>
+              <tr>
+                {parsedData &&
+                  parsedData[0].map((header, i) => (
+                    <th key={`header${i}`}>{header}</th>
+                  ))}
+              </tr>
+            </thead>
+            <tbody>
               {parsedData &&
-                parsedData[0].map((header, i) => (
-                  <th key={`header${i}`}>{header}</th>
-                ))}
-            </tr>
-          </thead>
-          <tbody>
-            {parsedData &&
-              parsedData.slice(1).map((row, i) => {
-                return (
-                  <tr key={`column${i}`}>
-                    {row.map((entry, j) => (
-                      <td key={`column${i}row${j}`}>{entry}</td>
-                    ))}
-                  </tr>
-                );
-              })}
-          </tbody>
-        </Table>
+                parsedData.slice(1).map((row, i) => {
+                  return (
+                    <tr key={`column${i}`}>
+                      {row.map((entry, j) => (
+                        <td key={`column${i}row${j}`}>{entry}</td>
+                      ))}
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
   );
